2bbdd229832bab9c310bddbb7974fe042a5ad7aa
Vladyslav Kupchuk <kupchuk.vlad@gmail.com>
2023-03-02 01:29:02 +0200

    markup of Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import Profile from 'components/Profile/Profile';
+import user from 'user.json';
+
+const { username, tag, location, avatar, stats } = user;
+
+const App = () => {
+	return (
+		<div>
+			<Profile
+				username={username}
+				tag={tag}
+				location={location}
+				avatar={avatar}
+				stats={stats}
+			/>
+		</div>
+	);
 };
+
+export default App;
